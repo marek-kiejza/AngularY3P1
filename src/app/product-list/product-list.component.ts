02f89559ae924f296a50f3f0de9e460e26ee1d59
Marek Kiejza <Marek.Kiejza@mail.itsligo.ie>
2018-09-24 09:58:49 +0100

    Add files via upload

---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent  {
+pageTitle: string ="Joe's Products";
+imageWidth: number = 50;
+imageMargin: number = 2;
+showImage: boolean = false;
+
+products: any[] = [
+  {
+      "productId": 2,
+      "productName": "Garden Cart",
+      "productCode": "GDN-0023",
+      "releaseDate": "March 18, 2016",
+      "description": "15 gallon capacity rolling garden cart",
+      "price": 32.99,
+      "starRating": 4.2,
+      "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
+  },
+  {
+      "productId": 5,
+      "productName": "Hammer",
+      "productCode": "TBX-0048",
+      "releaseDate": "May 21, 2016",
+      "description": "Curved claw steel hammer",
+      "price": 8.9,
+      "starRating": 4.8,
+      "imageUrl": "http://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
+  }
+];
+  toggleImage():void{
+    this.showImage = this.showImage;
+  }
+}
