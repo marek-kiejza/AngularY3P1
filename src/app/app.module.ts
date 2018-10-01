596d8be3f0616e88baf88aabb63fc9693c87835b
S00173510 <S00173510@VLAB-PC-72>
2018-10-01 13:35:34 +0100

    comit_3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { ConvertToSpaces } from './shared/convert-to-spaces-pipe';
+import { StarRatingComponent } from './shared/star-rating/star-rating.component';
+
+library.add(faStar);
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    ConvertToSpaces,
+    StarRatingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    FontAwesomeModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,17 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from './product';
+import { registerContentQuery } from '@angular/core/src/render3/instructions';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent  {
-pageTitle: string ="Joe's Products";
-imageWidth: number = 50;
-imageMargin: number = 2;
-showImage: boolean = false;
+export class ProductListComponent implements OnInit {
+  pageTitle: string ="Joe's Products";
+  imageWidth: number = 50;
+  imageMargin: number = 2;
+  showImage: boolean = false;
 
-products: any[] = [
+  _listFilter: string;
+  get listFilter(): string {
+    return this._listFilter;
+  }
+
+  set listFilter(value:string) {
+    this._listFilter = value;
+    this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter):this.products;
+  }
+
+  filteredProducts: IProduct[];
+  products: IProduct[] = [
+    {
+      "productId": 1,
+      "productName": "Leaf Rake",
+      "productCode": "GDN-0011",
+      "releaseDate": "March 19, 2016",
+      "description": "Leaf rake with 48-inch wooden handle.",
+      "price": 19.95,
+      "starRating": 3.2,
+      "imageUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
+  },
   {
       "productId": 2,
       "productName": "Garden Cart",
@@ -31,9 +54,42 @@ products: any[] = [
       "price": 8.9,
       "starRating": 4.8,
       "imageUrl": "http://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
+  },
+  {
+      "productId": 8,
+      "productName": "Saw",
+      "productCode": "TBX-0022",
+      "releaseDate": "May 15, 2016",
+      "description": "15-inch steel blade hand saw",
+      "price": 11.55,
+      "starRating": 3.7,
+      "imageUrl": "http://openclipart.org/image/300px/svg_to_png/27070/egore911_saw.png"
+  },
+  {
+      "productId": 10,
+      "productName": "Video Game Controller",
+      "productCode": "GMG-0042",
+      "releaseDate": "October 15, 2015",
+      "description": "Standard two-button video game controller",
+      "price": 35.95,
+      "starRating": 4.6,
+      "imageUrl": "http://openclipart.org/image/300px/svg_to_png/120337/xbox-controller_01.png"
   }
-];
-  toggleImage():void{
-    this.showImage = this.showImage;
+  ];
+
+  constructor(){
+    this.filteredProducts = this.products;
+  }
+  performFilter(filterBy:string):IProduct[]{
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.products.filter((product:IProduct)=> product.productName.toLocaleLowerCase().indexOf(filterBy) !=-1);
+  }
+
+  toggleImage(): void{
+    this.showImage = !this.showImage;
+  }
+
+  ngOnInit(): void {
+    console.log('In OnInit');
   }
 }
